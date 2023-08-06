cfc5fd36a021c568334ecec5d1b23e40e15a77ab
Digi | Toxic <DigiToxic55@gmail.com>
2023-08-06 17:26:57 -0400

    official

--- pages/login.jsx
+++ pages/login.jsx
@@ -11,25 +11,25 @@ export default function Login() {
     <div>
       <div className="flex justify-between items-center py-4 px-6 bg-black bg-opacity-0 border-purple-500 hover:bg-opacity-40 hover:border-b-2 hover:shadow-sm hover:shadow-purple-500 ease-in-out duration-200">
         <ThirdwebGuideHeader />
-        <div className="flex justify-between items-center w-72 ml-24">
+        <div className="flex justify-between items-center w-72 ml-24 max-[675px]:w-0">
           <p className="text-[rgb(215,183,250)] text-lg hover:text-purple-500 underline ease-in-out duration-200 cursor-pointer">
             <Link href="/">Gated</Link>
           </p>
-          <p className="text-white text-lg hover:text-purple-500 underline ease-in-out duration-200 cursor-pointer">
+          <p className="text-white text-lg hover:text-purple-500 underline ease-in-out duration-200 cursor-pointer max-[675px]:hidden">
             Lorem
           </p>
-          <p className="text-white text-lg hover:text-purple-500 underline ease-in-out duration-200 cursor-pointer">
+          <p className="text-white text-lg hover:text-purple-500 underline ease-in-out duration-200 cursor-pointer max-[675px]:hidden">
             Ipsum
           </p>
         </div>
 
         <div className="flex flex-col items-center justify-center">
           {address ? (
-            <p className="text-white">
+            <p className="text-white max-[500px]:hidden">
               Welcome, {address?.slice(0, 6)}...{address?.slice(-4)}
             </p>
           ) : (
-            <p className="text-white">Please connect your wallet.</p>
+            <p className="text-white max-[500px]:hidden">Please connect your wallet.</p>
           )}
 
           <ConnectWallet
@@ -40,8 +40,8 @@ export default function Login() {
       </div>
 
       <div>
-        <div className="flex items-center justify-evenly h-screen">
-          <div className="w-[30%] text-center mb-44">
+        <div className="flex items-center justify-evenly h-screen max-[675px]:flex-col">
+          <div className="w-[30%] text-center mb-44 max-[900px]:w-[40%] max-[900px]:mt-6 max-[675px]:mb-5 max-[675px]:w-5/6">
             <BlurredText text="This is a small showcase of a token gated website. Please feel free to try it out!" />
             <div className="mt-10">
               <BlurredText text="Also don't forget to follow me on twitter and checking my developer agency where we take your ideas to the next level!" />
@@ -50,7 +50,7 @@ export default function Login() {
 
           <div className="mb-32">
             <img
-              className="rounded-3xl w-96 shadow-lg shadow-[rgb(147,51,234)] hover:shadow-xl hover:shadow-[rgb(147,51,234)] ease-in-out duration-200"
+              className="rounded-3xl w-96 shadow-lg shadow-[rgb(147,51,234)] hover:shadow-xl hover:shadow-[rgb(147,51,234)] ease-in-out duration-200 max-[900px]:w-72 max-[675px]:w-64"
               src="/logo.png"
               alt=""
             />
